b5f10bd658aa385702c8b4a6c1559731828ce257
PriyamJeet <priyam.saluja36@gmail.com>
2017-02-07 20:05:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var articles = `{
+ articleone : {
+    title :"my first article"
+ }
+ articletwo : {
+    title :"my second article"
+ }
+ articlethree : {
+    title :"my thirs article"
+ }
+}`;
 
+function gethtmltemplate(data){
+    var title = data.title;
+var htmpltemplate = `<html>
+    <head>
+         <link rel="stylesheet" type="text/css" href="ui/style.css">
+    <title>$title</title>
+    </head>
+    
+    
+    <body>
+        <div class ="container">
+        <a href="/" >Home </a>
+        </div>
+    </body>
+    </html>`;
+    return htmpltemplate;
+    
+}
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/:articleName', function (req, res) {
+   var articleName = req.params.articleName;
+  res.sendFile(gethtmltemplate(articles[articleame]));
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -15,6 +48,10 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+app.get('/:article-name', function (req, res) {
+    var articlename = req.
+  res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
